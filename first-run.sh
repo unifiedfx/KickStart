9beb6180161ae7c1058c5bd1746c82c491da5bad
stephenwelsh <stephen.welsh@unifiedfx.com>
2024-04-10 00:59:50 +0100

    Create first-run.sh

---
+++ first-run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo dnf -y install yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo systemctl start docker
+sudo systemctl enable docker
+

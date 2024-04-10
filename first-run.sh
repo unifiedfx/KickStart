4e2dd0ffae1b48d65b604bdddf09ec0fb81ea7cd
stephenwelsh <stephen.welsh@unifiedfx.com>
2024-04-10 01:16:00 +0100

    Update first-run.sh

--- first-run.sh
+++ first-run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Installing Docker!"
+
 sudo dnf -y install yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
